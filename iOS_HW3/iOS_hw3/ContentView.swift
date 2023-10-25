4c15b2e117fd769f32851da59670bc2c91e2eeba
Ce1Y <sycn02430@gmail.com>
2023-10-25 10:21:35 +0800

    change homwork name

---
+++ iOS_HW3/iOS_hw3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_HW3
+//
+//  Created by user06 on 2023/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_HW3/iOS_hw3/iOS_hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_HW3App.swift
+//  iOS_HW3
+//
+//  Created by user06 on 2023/10/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
